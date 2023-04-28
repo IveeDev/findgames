e349864bf9be5c7728d4f3116af8fa118baa906f
Idoko Innocent <iidoko899@gmail.com>
2023-04-28 20:20:41 +0100

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,10 +3,17 @@ import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 // import { Game } from "../components/GameGrid";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGameResponse {
