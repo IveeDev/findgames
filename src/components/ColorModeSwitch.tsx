8579a50b413c2158f79a12be9ae72a85f4dbaa7d
Idoko Innocent <iidoko899@gmail.com>
2023-04-30 16:49:18 +0100

    Build search Input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -10,7 +10,7 @@ const ColorModeSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text>Dark mode</Text>
+      <Text whiteSpace="nowrap">Dark mode</Text>
     </HStack>
   );
 };
