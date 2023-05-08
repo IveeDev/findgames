1db922c13b1c75cdc9f24a7f9564f2f4e0a9c776
Idoko Innocent <iidoko899@gmail.com>
2023-05-08 09:45:45 +0100

    Set up routing

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,27 +1,22 @@
-import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
-import GameGrid from "./components/GameGrid";
-import GameHeading from "./components/GameHeading";
-import GenreList from "./components/GenreList";
-import NavBar from "./components/NavBar";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
+import { Box, Grid, Show, GridItem, Flex } from "@chakra-ui/react";
+import GameGrid from "../components/GameGrid";
+import GameHeading from "../components/GameHeading";
+import GenreList from "../components/GenreList";
+import PlatformSelector from "../components/PlatformSelector";
+import SortSelector from "../components/SortSelector";
 
-function App() {
+const HomePage = () => {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`, // Smaller screen
-        lg: `"nav nav" "aside main"`, // Larger Screen
+        base: `"main"`, // Smaller screen
+        lg: `"aside main"`, // Larger Screen
       }}
       templateColumns={{
         base: "1fr",
         lg: "200px 1fr", //aside takes 200px width while the other(Where we have our grid) expands
       }}
     >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
           <GenreList />
@@ -42,6 +37,6 @@ function App() {
       </GridItem>
     </Grid>
   );
-}
+};
 
-export default App;
+export default HomePage;
